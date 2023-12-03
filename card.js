01af748aa5bd22e6c3d4a728511217d6cbfa3dd8
NotoriousKaren <144821347+NotoriousKaren@users.noreply.github.com>
2023-12-03 21:54:14 +0800

    Update card.js

--- card.js
+++ card.js
@@ -5,30 +5,35 @@ let lockBoard = false;
 let firstCard, secondCard;
 
 function flipCard() {
-  if (lockBoard) return;
-  if (this === firstCard) return;
+  if (lockBoard) return; //wait for the first set of cards to finish
+  if (this === firstCard) return; //to remove the is matched function when double clicked
 
-  this.classList.add('flip');
+  this.classList.add('flip'); //flip sound for first & second card
   playFlipSound();
+  
   //flip first card
   if (!FlippedCard) {
     FlippedCard = true;
     firstCard = this;
     return;
   }
+  
   //flip second card
   secondCard = this;
   checkForMatch();
 }
 
-//Check for matching cards
+//check for matched cards
 function checkForMatch() {
   let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
 
+//ask if the cards matched
+  //ternary operator: (condition? true : false)
   isMatch ? disableCards() : unflipCards();
 }
 
 function disableCards() {
+     //if the cards matched remove eventlistener
   firstCard.removeEventListener('click', flipCard);
   secondCard.removeEventListener('click', flipCard);
 
@@ -43,18 +48,18 @@ function disableCards() {
 
 function unflipCards() {
   lockBoard = true;
-
+  //unflipped the cards 
   setTimeout(() => {
     firstCard.classList.remove('flip');
     secondCard.classList.remove('flip');
     playFlipSound();
 
     resetBoard();
-  }, 500);
+  }, 500); //set delay to see the other card flip
 }
 
-function resetBoard() {
-  [FlippedCard, lockBoard] = [false, false];
+function resetBoard() { //reset board to return the first card
+  [FlippedCard, lockBoard] = [false, false]; //lock the cards in place befoer flipping another
   [firstCard, secondCard] = [null, null];
 }
 
@@ -67,36 +72,36 @@ function restartGame() {
 
   // Reset all cards to their initial order
   cards.forEach(card => {
-    let randomPos = Math.floor(Math.random() * 12);
+    let randomPos = Math.floor(Math.random() * 12); //generate a random number to assign each of the card
     card.style.order = randomPos;
     card.classList.remove('flip');
     card.addEventListener('click', flipCard);
   });
 }
 
-function playFlipSound() {
+function playFlipSound() { //flip sound
   let flipSound = new Audio('audio/flipCard.wav');
   flipSound.play();
 }
 
-function playVictorySound() {
+function playVictorySound() { //victory sound
   let victorySound = new Audio('audio/success.wav');
   audioController.stopMusic(); 
   victorySound.play();
 }
 
-function playBackgroundMusic() {
+function playBackgroundMusic() { //background music
   audioController.startMusic();
 }
 
-cards.forEach(card => card.addEventListener('click', flipCard));
+cards.forEach(card => card.addEventListener('click', flipCard)); //eventlistener for when the card is clicked, it will flip
 
-function ready() { //Opening / CLICK TO START
+function ready() { //Opening: CLICK TO START 
   let overlays = Array.from(document.getElementsByClassName('overlay-text'));
 
   overlays.forEach(overlay => {
-    overlay.addEventListener('click', () => {
-      overlay.classList.remove('visible');
+    overlay.addEventListener('click', () => { //click to start the game
+      overlay.classList.remove('visible'); // remove the visibility of the overlay and start the game
       restartGame();
       playBackgroundMusic(); //Call out function for the bgMusic
     });
@@ -109,18 +114,18 @@ class AudioController { //Audio Control for the bgMusic
     this.bgMusic.volume = 0.7;
   }
 
-  startMusic() {
+  startMusic() { //starts the audio/music
     this.bgMusic.play();
   }
-  stopMusic(){
+  stopMusic(){  //stops the audio/music
     this.bgMusic.pause();
-    this.bgMusic.currentTime = 0;
+    this.bgMusic.currentTime = 0; //set time for the bgMusic to start
   }
 }
 
-let audioController = new AudioController();
+let audioController = new AudioController(); //to take all audio and call each of the audio
 
-if (document.readyState === 'loading') {
+if (document.readyState === 'loading') { //this function is when the html is finished loading called this condition (ready)
   document.addEventListener('DOMContentLoaded', ready);
 } else {
   ready();
